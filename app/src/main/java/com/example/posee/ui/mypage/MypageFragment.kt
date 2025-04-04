5f9264aaba37fea53af8d9d2140002100d73195e
hyhy0623 <rlagkdus0623@naver.com>
2025-04-04 13:35:55 +0900

    feat: Add stretching chart into MYpageFragment using ViewModel

--- app/src/main/java/com/example/posee/ui/mypage/MypageFragment.kt
+++ app/src/main/java/com/example/posee/ui/mypage/MypageFragment.kt
@@ -5,9 +5,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.posee.R
 import com.example.posee.databinding.FragmentNotificationsBinding
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.data.LineDataSet
 
 class MypageFragment : Fragment() {
 
@@ -27,6 +31,23 @@ class MypageFragment : Fragment() {
 
         _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
         val root: View = binding.root
+
+        // 차트 연결
+        val chart = binding.myChart
+
+        mypageViewModel.chartData.observe(viewLifecycleOwner) { entries ->
+            val dataSet = LineDataSet(entries, "Stretching").apply {
+                color = ContextCompat.getColor(requireContext(), R.color.main)
+                valueTextColor = ContextCompat.getColor(requireContext(), R.color.black)
+                lineWidth = 2f
+                setDrawCircles(true)
+                circleRadius = 4f
+            }
+
+            val lineData = LineData(dataSet)
+            chart.data = lineData
+            chart.invalidate()
+        }
 
         //val textView: TextView = binding.textNotifications
         mypageViewModel.text.observe(viewLifecycleOwner) {
--- app/src/main/java/com/example/posee/ui/mypage/MypageViewModel.kt
+++ app/src/main/java/com/example/posee/ui/mypage/MypageViewModel.kt
@@ -3,6 +3,7 @@ package com.example.posee.ui.mypage
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.github.mikephil.charting.data.Entry
 
 class MypageViewModel : ViewModel() {
 
@@ -10,4 +11,23 @@ class MypageViewModel : ViewModel() {
         value = ""
     }
     val text: LiveData<String> = _text
+
+    // 차트 데이터 추가
+    private val _chartData = MutableLiveData<List<Entry>>()
+    val chartData: LiveData<List<Entry>> = _chartData
+
+    init {
+        loadChartData()
+    }
+
+    private fun loadChartData() {
+        val entries = listOf(
+            Entry(1f, 1f),
+            Entry(2f, 2f),
+            Entry(3f, 0f),
+            Entry(4f, 4f),
+            Entry(5f, 3f)
+        )
+        _chartData.value = entries
+    }
 }
